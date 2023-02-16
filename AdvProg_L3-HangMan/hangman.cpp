1c4e524369977937253bdb2b2e81bcb9c2a06591
giangbang <banggiangle2015@gmail.com>
2023-02-17 00:01:45 +0700

    fix bug in assignment L3

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -104,7 +104,7 @@ void updateSecretWord(string& secretWord, const char ch, const string& word)
 {
     // TODO: Update the secret word if the character ch is in the answer word.
     for (int i = 0; i < secretWord.length(); ++i) {
-        if (ch == secretWord[i]) word[i] = ch;
+        if (ch == secretWord[i]) secretWord[i] = ch;
     }
 }
 
